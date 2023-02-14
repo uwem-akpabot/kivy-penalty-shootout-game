4090b265e5f5f47932268f9c7e74e8a206b422b2
Uwem Akpabot <akpabot.it@gmail.com>
2023-02-14 16:15:52 +0100

    added 13022023

--- backup_main.py
+++ backup_main.py
@@ -2,10 +2,10 @@ from kivy.app import App
 from kivy.animation import Animation
 
 class MainApp(App):
-    title = "Sustainable Development Goals"
+    title = "My very good game"
 
     def animate_ball(self, widget, *args):
-        anim = Animation(top_hint=.3, duration=0.2) # change here for top_hint=1 and ball duration=0.2 
+        anim = Animation(top_hint=.3, duration=2) # change here for top_hint=1 and ball duration=0.2 
         anim.bind(on_progress=self.my_progress_callback)
         anim.start(widget)
 
